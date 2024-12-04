197951f512ca152576b5e6515062f1bfa0e0cbf8
Pankaj Bhattarai <pankajbhattarai50@gmail.com>
2024-12-04 15:42:28 +0545

    bod pages complete but link updation left

--- src/pages/bod.js
+++ src/pages/bod.js
@@ -7,6 +7,18 @@ import "../assets/styles/bod.css";
 
 import nitamImage from '../assets/images/team/nitam.png';
 import monikaImage from "../assets/images/team/monika.png"
+import pankajImage from "../assets/images/team/pankaj.png"
+import abhinabImage from "../assets/images/team/abhinab.png"
+import shekharImage from "../assets/images/team/shekhar.jpg"
+import vijayaImage from "../assets/images/team/vijaya.png"
+import ronikImage from "../assets/images/team/ronik.jpeg"
+import ashleshaImage from "../assets/images/team/ashlesha.png"
+import siddhantImage from "../assets/images/team/siddhant.jpg"
+import nirojImage from "../assets/images/team/niroj.png"
+import shrisnaImage from "../assets/images/team/shrisna.jpeg"
+import sujayImage from "../assets/images/team/sujay.png"
+import pratikImage from "../assets/images/team/pratik.jpg"
+
 const BOD = () => {
   const teamMembers = [
     {
@@ -14,15 +26,15 @@ const BOD = () => {
       frontImage: nitamImage,
       frontName: "Nitamraj Poudel",
       frontTitle: "President",
-      backText: 
+      backText:
         <div class="social-icons">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.facebook.com/nitamrajpoudel" target="_blank" rel="noopener noreferrer">
             <i class="fab fa-facebook"></i>
           </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/_nitamraj_/" target="_blank" rel="noopener noreferrer">
             <i class="fab fa-instagram"></i>
           </a>
-          <a href="mailto:nitam@example.com">
+          <a href="mailto:nitam@kalpabriksha.org">
             <i class="fas fa-envelope"></i>
           </a>
         </div>
@@ -33,7 +45,7 @@ const BOD = () => {
       frontImage: monikaImage,
       frontName: "Monika Shrestha",
       frontTitle: "Vice-President",
-      backText: 
+      backText:
         <div class="social-icons">
           <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
             <i class="fab fa-facebook"></i>
@@ -47,8 +59,215 @@ const BOD = () => {
         </div>
       ,
     },
-    
-    // Add more members here as needed...
+    {
+      id: 3,
+      frontImage: pankajImage,
+      frontName: "Pankaj Bhattarai",
+      frontTitle: "General Secretary",
+      backText:
+        <div class="social-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="mailto:monika@example.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      ,
+    },
+    {
+      id: 4,
+      frontImage: abhinabImage,
+      frontName: "Abhinab Khanal",
+      frontTitle: "Secretary",
+      backText:
+        <div class="social-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="mailto:monika@example.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      ,
+    },
+    {
+      id: 5,
+      frontImage: shekharImage,
+      frontName: "Shekhar Thapa",
+      frontTitle: "Treasurer",
+      backText:
+        <div class="social-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="mailto:monika@example.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      ,
+    },
+    {
+      id: 6,
+      frontImage: vijayaImage,
+      frontName: "Vijaya Laxmi Sharma",
+      frontTitle: "Executive",
+      backText:
+        <div class="social-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="mailto:monika@example.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      ,
+    },
+    {
+      id: 7,
+      frontImage: ronikImage,
+      frontName: "Ronik Basnet",
+      frontTitle: "Executive",
+      backText:
+        <div class="social-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="mailto:monika@example.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      ,
+    },
+    {
+      id: 8,
+      frontImage: ashleshaImage,
+      frontName: "Ashlesha Shrestha",
+      frontTitle: "Executive",
+      backText:
+        <div class="social-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="mailto:monika@example.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      ,
+    },
+    {
+      id: 9,
+      frontImage: siddhantImage,
+      frontName: "Siddhant Silwal",
+      frontTitle: "Executive",
+      backText:
+        <div class="social-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="mailto:monika@example.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      ,
+    },
+    {
+      id: 10,
+      frontImage: nirojImage,
+      frontName: "Niroj Neupane Khatri",
+      frontTitle: "Executive",
+      backText:
+        <div class="social-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="mailto:monika@example.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      ,
+    },
+    {
+      id: 11,
+      frontImage: shrisnaImage,
+      frontName: "Shrisna Thapa",
+      frontTitle: "Executive",
+      backText:
+        <div class="social-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="mailto:monika@example.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      ,
+    },
+    {
+      id: 12,
+      frontImage: sujayImage,
+      frontName: "Sujay Kumar Pal",
+      frontTitle: "Executive",
+      backText:
+        <div class="social-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="mailto:monika@example.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      ,
+    },
+    {
+      id: 13,
+      frontImage: pratikImage,
+      frontName: "Pratik Luitel",
+      frontTitle: "Executive",
+      backText:
+        <div class="social-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="mailto:monika@example.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      ,
+    },
   ];
 
   return (
@@ -73,6 +292,7 @@ const BOD = () => {
           ))}
         </div>
       </div>
+
       <Footer />
     </div>
   );
