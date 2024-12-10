6a2ef479151fa89e8116f39e4bac1943e767f4cc
Pankaj Bhattarai <pankajbhattarai50@gmail.com>
2024-12-10 16:53:08 +0545

    fixed about css

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/navbar";
 import HeaderSeparator from "../components/HeaderSeparator";
 import Footer from "../components/footer";
-// import "../assets/styles/about.css";
+import "../assets/styles/about.css";
 import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
 import FlippingCard from '../components/FlippingCard';
 
