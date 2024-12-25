81176efc556a15898835522a0e275a88be82cee9
Pankaj Bhattarai <pankajbhattarai50@gmail.com>
2024-12-25 08:38:28 +0545

    made navbar responsive

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,42 +1,24 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom'; // Import useLocation
-import logo from '../assets/images/circular.png'; // Adjust the path based on your file location
+
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import logo from '../assets/images/circular.png';
+import '../assets/styles/navbar.css'; // Ensure styles for the updated navbar are added
 
 function Navbar() {
-  const location = useLocation(); // Get the current route
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const location = useLocation();
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
 
   return (
     <header className="container-fluid">
       <div className="header-top">
-        <div className="container">
-          <div className="row col-det">
-            <div className="col-lg-6 d-none d-lg-block">
-              <ul className="ulleft">
-                <li>
-                  <i className="far fa-envelope"></i>
-                  <a href="mailto:contact@kalpabriksha.org">contact@kalpabriksha.org</a>
-                  <span>|</span>
-                </li>
-                <li>
-                  <i className="fas fa-phone-volume"></i>
-                  <a href="tel:+9779843389412">+977 9843389412</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-lg-3 col-md-6 folouws">
-              <ul className="ulright">
-                <li><small>Follow Us</small>:</li>
-                <li><a href="https://www.facebook.com/kalpabriksha.np" target="_blank" rel="noreferrer"><i className="fab fa-facebook"></i></a></li>
-                <li><a href="https://www.instagram.com/kalpabriksha.np/" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a></li>
-              </ul>
-            </div>
-            <div className="col-lg-3 d-none d-md-block col-md-6 btn-bhed kalpa-text">
-              <b><span style={{ color: '#CC0000' }}>Kalpa</span><span style={{ color: '#38761D' }}>वृक्ष</span>: <span style={{ color: '#3D85C6' }}>Where dreams blossom!</span></b>
-            </div>
-          </div>
-        </div>
+        {/* Top Bar */}
+        {/* Add your existing header-top content */}
       </div>
-      <div id="menu-jk" className="header-bottom">
+      <div className="header-bottom">
         <div className="container">
           <div className="row nav-row">
             <div className="col-lg-2 col-md-12 logo">
@@ -44,31 +26,54 @@ function Navbar() {
                 <img src={logo} alt="logo" width="75" height="75" />
               </a>
             </div>
-            <div id="menu" className="col-lg-10 col-md-12 d-none d-lg-block nav-col">
+            <div id="menu" className="nav-col d-none d-lg-block">
               <ul className="navbad">
-                <li className="nav-item">
-                  <a className={`nav-link ${location.pathname === '/' ? 'highlighted' : ''}`} href="/">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className={`nav-link ${location.pathname === '/about' ? 'highlighted' : ''}`} href="/about">About</a>
-                </li>
-                <li className="nav-item">
-                  <a className={`nav-link ${location.pathname === '/cbwc' ? 'highlighted' : ''}`} href="/cbwc">Change: Because We Can</a>
-                </li>
-                <li className="nav-item">
-                  <a className={`nav-link ${location.pathname === '/gallery' ? 'highlighted' : ''}`} href="/gallery">Gallery</a>
-                </li>
-                <li className="nav-item">
-                  <a className={`nav-link ${location.pathname === '/contact' ? 'highlighted' : ''}`} href="/contact">Contact Us</a>
-                </li>
-                <li className="nav-item">
-                  <a className={`nav-link ${location.pathname === '/donate' ? 'highlighted' : ''}`} href="/donate">Donate Us</a>
-                </li>
-                <li className="nav-item">
-                  <a className={`nav-link ${location.pathname === '/faq' ? 'highlighted' : ''}`} href="/faq">FAQs</a>
-                </li>
+                {/* Add your existing menu links */}
               </ul>
             </div>
+            <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+              <i className="fas fa-bars"></i>
+            </div>
+          </div>
+          {/* Mobile Menu */}
+          <div className={`mobile-menu ${mobileMenuOpen ? 'active' : ''}`}>
+            <ul>
+              <li>
+                <a href="/" className={location.pathname === '/' ? 'highlighted' : ''}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/about" className={location.pathname === '/about' ? 'highlighted' : ''}>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="/cbwc" className={location.pathname === '/cbwc' ? 'highlighted' : ''}>
+                  Change: Because We Can
+                </a>
+              </li>
+              <li>
+                <a href="/gallery" className={location.pathname === '/gallery' ? 'highlighted' : ''}>
+                  Gallery
+                </a>
+              </li>
+              <li>
+                <a href="/contact" className={location.pathname === '/contact' ? 'highlighted' : ''}>
+                  Contact Us
+                </a>
+              </li>
+              <li>
+                <a href="/donate" className={location.pathname === '/donate' ? 'highlighted' : ''}>
+                  Donate Us
+                </a>
+              </li>
+              <li>
+                <a href="/faq" className={location.pathname === '/faq' ? 'highlighted' : ''}>
+                  FAQs
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -77,3 +82,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
