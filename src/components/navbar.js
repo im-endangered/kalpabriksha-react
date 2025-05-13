dac7af201c7e44aa970bf18e0cda091d6f2db8f3
Pankaj Bhattarai <pankajbhattarai50@gmail.com>
2025-05-13 20:22:04 +0545

    navbar changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,115 +11,67 @@ function Navbar() {
   };
 
   return (
-    <header className="container-fluid">
-      <div className="header-top">
-        <div className="container">
-          <div className="row col-det">
-            <div className="col-lg-6 d-none d-lg-block">
-              <ul className="ulleft">
-                <li>
-                  <i className="far fa-envelope"></i>
-                  <a href="mailto:contact@kalpabriksha.org">contact@kalpabriksha.org</a>
-                  <span>|</span>
-                </li>
-                <li>
-                  <i className="fas fa-phone-volume"></i>
-                  <a href="tel:+9779843389412">+977 9763684743</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-lg-3 col-md-6 folouws">
-              <ul className="ulright">
-                <li>
-                  <small>Follow Us</small>:
-                </li>
-                <li>
-                  <a
-                    href="https://www.facebook.com/kalpabriksha.np"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fab fa-facebook"></i>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.instagram.com/kalpabriksha.np/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fab fa-instagram"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
+    <header className="navbar-container">
+      {/* Top Bar */}
+      <div className="navbar-top">
+        <div className="container d-flex justify-content-between align-items-center flex-wrap">
+          <div className="d-flex align-items-center contact-info">
+            <i className="far fa-envelope me-1"></i>
+            <a href="mailto:contact@kalpabriksha.org" className="me-3">contact@kalpabriksha.org</a>
+            <i className="fas fa-phone-volume me-1"></i>
+            <a href="tel:+9779763684743">+977 9763684743</a>
+          </div>
+          <div className="d-flex align-items-center social-links">
+            <small className="me-2">Follow Us:</small>
+            <a href="https://www.facebook.com/kalpabriksha.np" target="_blank" rel="noreferrer">
+              <i className="fab fa-facebook me-2"></i>
+            </a>
+            <a href="https://www.instagram.com/kalpabriksha.np/" target="_blank" rel="noreferrer">
+              <i className="fab fa-instagram"></i>
+            </a>
           </div>
         </div>
       </div>
-      <div id="menu-jk" className="header-bottom">
-        <div className="container">
-          <div className="row nav-row">
-            <div className="col-lg-2 col-md-12 logo">
-              <Link to="/">
-                <img src={logo} alt="logo" width="75" height="75" />
-              </Link>
-              <button
-                className={`small-menu d-lg-none ${
-                  isMenuOpen || location.pathname === "/" ? "bg-blue" : ""
-                }`}
-                onClick={toggleMenu}
-              >
-                &#9776;
-              </button>
-            </div>
-            <div
-              id="menu"
-              className={`col-lg-12 col-md-9 nav-col ${isMenuOpen ? "d-block" : "d-none d-lg-block"}`}
-            >
-              <ul className="navbad">
-                <li>
-                  <NavLink to="/" className={({isActive})=>`nav-link ${isActive ? "highlighted" : ""}`} exact>
-                    Home
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/about" className={({isActive})=>`nav-link ${isActive ? "highlighted" : "nav-link"}`}>
-                    About
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/cbwc" className={({isActive})=>`nav-link ${isActive ? "highlighted" : "nav-link"}`}>
-                    Change: Because We Can
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/gallery" className={({isActive})=>`nav-link ${isActive ? "highlighted" : "nav-link"}`}>
-                    Gallery
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/contact" className={({isActive})=>`nav-link ${isActive ? "highlighted" : "nav-link"}`}>
-                    Contact Us
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/donate" className={({isActive})=>`nav-link ${isActive ? "highlighted" : "nav-link"}`}>
-                    Donate Us
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/faq" className={({isActive})=>`nav-link ${isActive ? "highlighted" : "nav-link"}`}>
-                    FAQs
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/download" className={({isActive})=>`nav-link ${isActive ? "highlighted" : "nav-link"}`}>
-                    Downloads
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
+
+      {/* Bottom Navigation */}
+      <div className="navbar-main">
+        <div className="container d-flex justify-content-between align-items-center flex-wrap">
+          {/* Logo */}
+          <div className="d-flex align-items-center">
+            <Link to="/" className="navbar-logo me-3">
+              <img src={logo} alt="Kalpabriksha Logo" height="55" />
+            </Link>
+            <button className="d-lg-none nav-toggle" onClick={toggleMenu}>
+              &#9776;
+            </button>
           </div>
+
+          {/* Nav Links */}
+          <nav className={`nav-links ${isMenuOpen ? "show" : ""}`}>
+            <ul className="d-flex flex-wrap align-items-center justify-content-center">
+              {[
+                { path: "/", name: "Home" },
+                { path: "/about", name: "About" },
+                { path: "/cbwc", name: "Change: Because We Can" },
+                { path: "/gallery", name: "Gallery" },
+                { path: "/contact", name: "Contact Us" },
+                { path: "/donate", name: "Donate Us" },
+                { path: "/faq", name: "FAQs" },
+                { path: "/download", name: "Downloads" },
+              ].map((link) => (
+                <li key={link.path} className="nav-item">
+                  <NavLink
+                    to={link.path}
+                    className={({ isActive }) =>
+                      `nav-link ${isActive ? "highlighted" : ""}`
+                    }
+                  >
+                    {link.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
